2d2deb80076109bc0ff559a22e63a0be6e665ea6
Venusai <118623801+Venu005@users.noreply.github.com>
2023-10-30 16:02:13 +0000

    message

--- vector_basics/vector_2d.cpp
+++ vector_basics/vector_2d.cpp
@@ -5,19 +5,22 @@ int main(){
 
    //2d vector 
    std::vector <std::vector <int>> twod_vector {{1,2,3},{4,5,6},{7,8,9}};
-   std::cout << twod_vector.at(0).at(0)<< std::endl;
-   std::vector <int> second_row =  twod_vector.at(1);
-   for(int element : second_row){
-    std::cout << element;
-   }
+   //std::cout << twod_vector.at(0).at(0)<< std::endl;
+  //  std::vector <int> second_row =  twod_vector.at(1);
+  //  for(int element : second_row){
+  //   std::cout << element;
+  //  }
+  for(auto firstrow : twod_vector.at(0)){
+    std::cout << firstrow ;
+  }
   
-   for(const std::vector<int> &first_col:twod_vector){
+   for(auto first_col : twod_vector){
     std::cout<< std::endl << first_col.at(0)<< std::endl;
    }
-   for(const std::vector<int> &whole_vector : twod_vector){
-     for(int element : whole_vector){
-        std::cout << element <<std::endl;
-    }
+    // for(auto vec: twod_vector){
+    //   for(auto elem : vec){
+    //     std::cout << elem << std::endl;
+    //   }
+    // }
 }
 
-}
