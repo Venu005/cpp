e624b61bf2b038eb192085d2d1b0df43ed309049
Venusai <118623801+Venu005@users.noreply.github.com>
2023-10-31 22:07:06 +0530

    Update vect_pointers.cpp

--- vector_basics/vect_pointers.cpp
+++ vector_basics/vect_pointers.cpp
@@ -23,3 +23,36 @@ int main() {
 
     return 0;
 }
+// CONST POINTERS WITH FUNCTIONS 
+/*
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+void display(const std::vector <std::string>  *const );
+void display(const std::vector<std::string>  *const v){
+    for (auto elem : *v){
+        std::cout << elem << " ";
+    }
+    //v->at(0)="kats";
+    //(*v).at(0)="kats";
+    //v = nullptr; // error as *v set to const 
+}
+
+int main(){
+    std::vector <std::string> names{"Venu","sat","meghs"};
+    display(&names);
+    display(&names);
+    
+}
+// void double_data(int *int_ptr){
+//     *int_ptr*=2;
+// }
+
+// int main(){
+//     int value{10};
+//     int *int_ptr{&value};
+//     double_data(int_ptr);
+//     std::cout << *int_ptr << std::endl; 
+*/  
